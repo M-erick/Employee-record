fdf703746bfa5e0cf0fe512002100b1d275db39f
Erick Muriithi <muriithierick3758@gmail.com>
2024-04-02 15:29:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+var selectedRow = null
+
+
+
+function readFormData() {
+    const fullName = document.getElementById("fullName").value;
+    const empCode = document.getElementById("empCode").value;
+    const salary = document.getElementById("salary").value;
+    const city = document.getElementById("city").value;
+
+    var formData = {
+        fullName,
+        empCode,
+        salary,
+        city
+    };
+
+
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    // add the table header here;to display only when there is data
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.empCode;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.city;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<button onClick="onEdit(this)" style ="border-radius:4px;background-color:#ddd;border:none; padding:14px 20px;">Edit</button>
+                       <button onClick="onDelete(this)" style="border-radius:4px;background-color:#ddd;border:none;padding:14px 20px;">Delete</button>`;
+}
+
+
+
+// Edit use records
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+}
+// update user records
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.empCode;
+    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[3].innerHTML = formData.city;
+}
+
+function onDelete(td) {
+    
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    
+}
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("empCode").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("city").value = "";
+    selectedRow = null;
+}
+function onFormSubmit() {
+    
+    var formData = readFormData();
+    if (selectedRow == null)
+        insertNewRecord(formData);
+    else
+        updateRecord(formData);
+    resetForm();
+
+}
