d98339538e773f68f3098a0ae188c6a2ec61231e
Erick Muriithi <muriithierick3758@gmail.com>
2024-04-03 10:32:40 +0300

    styled the pages

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-class EmployeeManager {
+class DataManager {
     constructor() {
+      this.data = null;
+    }
+  
+    insert(newData) {
+      throw new Error('Insert method not implemented');
+    }
+  
+    update(newData) {
+      throw new Error('Update method not implemented');
+    }
+  
+    delete() {
+      throw new Error('Delete method not implemented');
+    }
+  }
+class EmployeeManager extends DataManager {
+    constructor() {
+        super();
       this.selectedRow = null;
+      
     }
   
     readFormData() {
@@ -8,12 +27,16 @@ class EmployeeManager {
       const empCode = document.getElementById("empCode").value;
       const salary = document.getElementById("salary").value;
       const city = document.getElementById("city").value;
-  
+      const nhifRate = 0.01;
+      const nssfRate = 0.005;
+      const housingTax = 0.01;
+      const netSalary = salary - (salary * nhifRate) - (salary * nssfRate) - (salary * housingTax);
       return {
         fullName,
         empCode,
         salary,
-        city
+        city,
+        netSalary
       };
     }
   
@@ -27,9 +50,12 @@ class EmployeeManager {
       newRow.insertCell(1).innerHTML = data.empCode;
       newRow.insertCell(2).innerHTML = data.salary;
       newRow.insertCell(3).innerHTML = data.city;
-      newRow.insertCell(4).innerHTML = `
-        <button onClick="employeeManager.onEdit(this)" style ="border-radius:4px;background-color:#ddd;border:none; padding:14px 20px;">Edit</button>
-        <button onClick="employeeManager.onDelete(this)" style="border-radius:4px;background-color:#ddd;border:none;padding:14px 20px;">Delete</button>`;
+      newRow.insertCell(4).innerHTML = data.netSalary;
+
+
+      newRow.insertCell(5).innerHTML = `
+        <button onClick="employeeManager.onEdit(this)" style ="border-radius:4px;background-image: url('image.png'); color:white; border:none; padding:14px 20px;">Edit</button>
+        <button onClick="employeeManager.onDelete(this)" style="border-radius:4px;background-image: url('image.png'); color:white; border:none;padding:14px 20px;">Delete</button>`;
     }
   
     onEdit(td) {
@@ -45,6 +71,8 @@ class EmployeeManager {
       this.selectedRow.cells[1].innerHTML = formData.empCode;
       this.selectedRow.cells[2].innerHTML = formData.salary;
       this.selectedRow.cells[3].innerHTML = formData.city;
+      this.selectedRow.cells[4].innerHTML = formData.netSalary;
+
     }
   
     onDelete(td) {
