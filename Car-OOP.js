e0b3f118c08573d6fa8d4a652b97af7eb996eb0e
Erick Muriithi <muriithierick3758@gmail.com>
2024-04-06 08:23:10 +0300

    Promises in Js

--- Car-OOP.js
+++ Car-OOP.js
@@ -62,7 +62,8 @@ class Car {
     }
   
     makeNoise() {
-      console.log("The sedan is honking!"); 
+      // console.log("The sedan is honking!"); 
+      return  `Sedan model ${this.model} make ${this.year} ${this.brand}  is honking.`;
     }
   }
   
@@ -85,7 +86,9 @@ class Car {
     }
   
     makeNoise() {
-      console.log("The truck is blowing its air horn!"); // Specific noise for Truck
+      // console.log("The truck is blowing its air horn!"); // Specific noise for Truck
+      return  `Truck model ${this.model} make ${this.year} ${this.brand}  is blowing Air horn.`;
+
     }
   }
   
@@ -96,20 +99,98 @@ class Car {
 const createSedanButton = document.getElementById("createSedan");
 const createTruckButton = document.getElementById("createTruck");
 const carDetailsDiv = document.getElementById("carDetails");
+const aboutUs = document.getElementById("aboutUs");
+const displayAbout = document.getElementById("about")
+// custom Event:takes in the input and transforms it to upper case .
+const displayEvent = new Event("display");
 
-// Events in JavaScript
+  document.addEventListener('display', () => {
+    // console.log("Sedan Car is starting");
+    const carDetailsElement = document.getElementById("carDetails");
+    const carDetailsText = carDetailsElement.textContent;
+    const uppercaseDetails = carDetailsText.toUpperCase();
+    carDetailsElement.innerHTML = uppercaseDetails;
+  });
+
+
+
+
+  const DealEvent = new CustomEvent("start", {
+    detail: {
+      about: " Car people :We are a car rating site ..."
+    }
+});
+// To trigger the event Listener
+document.addEventListener('start', (e)=>{
+  const aboutUsDetails =displayAbout.textContent;
+   displayAbout.innerHTML = e.detail.about;
+    
+});
+aboutUs.addEventListener("click",function(){
+  document.dispatchEvent(DealEvent);
+})
+
+//  Built-in Events in JavaScript
 createSedanButton.addEventListener("click", function() {
   const newSedan = new Sedan("Honda", "Civic", 2023, 4);
-  displayCarDetails(newSedan);
+  // setTimeout(()=>{
+
+    displayCarDetails(newSedan);
+    document.dispatchEvent(displayEvent);
+
+
+  // },3000);
+  // dispatching the event
+
 });
+
+
 
 createTruckButton.addEventListener("click", function() {
   const newTruck = new Truck("Ford", "F-150", 2022, 8);
   displayCarDetails(newTruck);
+  // dispatching the event
+  document.dispatchEvent(displayEvent);
+
 });
 
+ let isCarDetailsDisplayed;
+
+
 function displayCarDetails(car) {
+
   carDetailsDiv.textContent = car.getDetails() + "\n" + car.makeNoise();
+  isCarDetailsDisplayed =true;
+
+
 }
+
+
+
+let p = new Promise((resolve, reject) => {
+  let t =1 + 3;
+  let info  = {name:"Benz",model:"C-250",year:2016};
+  // t===4
+  if(info ===null) {
+    reject("no data available");
+  }else {
+    setTimeout(()=>{    
+      resolve(info);
+    },5000);
+
+  }
   
   
+});
+
+p.then((info) => {
+  console.log(info);
+});
+
+let someVar; 
+
+someVar = 10; // Assigned a number
+console.log(someVar, typeof someVar); // Output: 10 number
+
+someVar = "Hello"; // Assigned a string
+console.log(someVar, typeof someVar); // Output: Hello string
